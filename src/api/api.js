9a0b5b7e61cfcde7162aa01a67c4b3ea0a1b4b70
Иван <istepanchikov@scid.ru>
2025-08-12 20:56:42 +0300

    dd

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,6 @@ import router from '@/router';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
-    timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
     },
